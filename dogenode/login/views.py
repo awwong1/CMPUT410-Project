247c5619bd3536135803c30b5b702b74b4a216ed
Tsung Lin Yong <tsunglin@ualberta.ca>
2014-02-25 09:45:55 -0700

    Changed login to use Django's authentication stuff

--- dogenode/login/views.py
+++ dogenode/login/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.template import RequestContext
 from login.models import Author
 
+from django.contrib.auth import authenticate
+
 # Create your views here.
 def index(request):
     context = RequestContext(request)
@@ -11,13 +13,17 @@ def index(request):
         username = request.POST['username']
         password = request.POST['password']
 
-        user = Author.objects.filter(username=username)
+        #https://docs.djangoproject.com/en/dev/topics/auth/default/#authenticating-users
+        user = authenticate(username=username, password=password)
 
-        if len(user) == 0:
-            return HttpResponse("No user named %s was found" % username)
-        elif user[0].password != password:
-            return HttpResponse("Password '%s' is incorrect" % password)
+        if user is not None:
+            # the password verified for the user
+            if user.is_active:
+                return HttpResponse("User is valid, active and authenticated")
+            else:
+                return HttpResponse("Account disabled (password correct)!")
         else:
-            return HttpResponse("Login successful!")
+            # Incorrect username and password
+            return HttpResponse("The username and password were incorrect.")
 
     return render(request, 'login/index.html', context)
