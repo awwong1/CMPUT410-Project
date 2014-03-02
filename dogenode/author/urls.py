91f3a8ba7825c3bce782d9f67017783c719b15aa
Bronte Lee <bronte@ualberta.ca>
2014-03-01 19:52:28 -0700

    friends, posts, search_results html sample pages up. Unstyled.

--- dogenode/author/urls.py
+++ dogenode/author/urls.py
@@ -3,12 +3,12 @@ from django.conf.urls import patterns, url
 from author import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^profile/$', views.profile, name='profile'),
-    #url(r'^profile/(?P<author_name>\w+)/$', views.index, name='profile'),
-    url(r'^profile/edit_profile$', views.edit_profile, name='edit_profile'),
-    url(r'^posts$', views.posts, name='your_posts'),
-    #url(r'^posts/?P<post_id>\w)/$', views.get_post, name='your_posts'),
-    url(r'^friends$', views.friends, name='your_friends'),
-    url(r'^search$', views.search, name='search_results'),
+    url(r'^$', views.index),
+    url(r'^profile/$', views.profile),
+    #url(r'^profile/(?P<author_name>\w+)/$', views.get_author),
+    url(r'^profile/edit_profile$', views.edit_profile),
+    url(r'^posts/$', views.posts),
+    #url(r'^posts/?P<post_id>\w)/$', views.get_post),
+    url(r'^friends/$', views.friends),
+    url(r'^search_results/$', views.search),
 )
