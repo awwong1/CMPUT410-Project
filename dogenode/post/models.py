79668de1106e570579a75cc5f44eab0c91d6cc78
sgil <sgil@ualberta.ca>
2014-03-04 11:19:43 -0700

    wrote post model

--- dogenode/post/models.py
+++ dogenode/post/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from author.models import Author
+from comment.models import Comment
 
 # Create your models here.
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    author = models.ForeignKey("author.Author")
+    content = models.TextField("post content")
+    comments = models.ManyToManyField("comments.Comment")
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    
--- dogenode/post/views.py
+++ dogenode/post/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext
 
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 # Create your views here.
+
+def posts(request):
+    context = RequestContext(request)
+    
+    return render(request, 'post/posts.html', context)
+
