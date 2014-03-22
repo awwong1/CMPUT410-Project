bd5a5f4a211616649f151266326bbb617b326718
Stephanie Gil <sgil@ualberta.ca>
2014-03-21 19:24:24 -0700

    can no longer make blank comments or posts, and restful author now matches example json

--- dogenode/api/serializers.py
+++ dogenode/api/serializers.py
@@ -7,7 +7,7 @@ from categories.models import Category
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
-        fields = ('user', 'accepted')
+        fields = ('user', 'host', 'displayName', 'url')
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
