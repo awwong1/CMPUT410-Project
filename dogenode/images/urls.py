d252fdd96adb197257f2c381bd33d870c2072831
ajyong <ajyong@ualberta.ca>
2014-03-24 03:38:56 -0600

    getImage URL regex redone
    Wasn't supporting all cases initially, but now I'm pretty confident
    it's doing what I want it to do.  Also supports junk after the ? mark
    to force browsers to send a new GET request.

--- dogenode/images/urls.py
+++ dogenode/images/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('',
      url(r'^$', views.getViewableImages),
      url(r'^upload/$', views.uploadImage),
      url(r'^(?P<author_guid>[-\w]+)/$', views.getImagesByAuthor),
-     url(r'^(?P<author_guid>[-\w]+)/(?P<image_name>[-\w\.]+)/$', views.getImage),
+     url(r'^(?P<author_guid>[-\w]+)/(?P<image_name>[^/\\:*?"<>|]+)\?*.*/$', views.getImage),
 )
