ea7c01008a2efdb7da10a55bf543bc8a2379676a
Tsung Lin Yong <tsunglin@ualberta.ca>
2014-04-04 15:01:49 -0600

    Add whitelist model in api
    This will contain the servers that are allowed to communicate
    with our server.

--- dogenode/api/models.py
+++ dogenode/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+# whitelist
+class AllowedServer(models.Model):
+    name = models.CharField(max_length=50, blank=True)
+    host = models.CharField(max_length=100, default="http://127.0.0.1:8000/")
